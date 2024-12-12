0b3a792f1d86b3b77dfba672088f488df364f0cf
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-12-12 09:55:11 +0100

    FIX: Adding views

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,7 +1,4 @@
-import { StyleSheet, Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import { StyleSheet } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -10,16 +7,16 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 export default function MessageScreen() {
 	return (
 		<ParallaxScrollView
-			headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+			headerBackgroundColor={ {light: '#D0D0D0', dark: '#353636'} }
 			headerImage={
 				<IconSymbol
-					size={310}
+					size={ 310 }
 					color="#808080"
 					name="chevron.left.forwardslash.chevron.right"
-					style={styles.headerImage}
+					style={ styles.headerImage }
 				/>
 			}>
-			<ThemedView style={styles.titleContainer}>
+			<ThemedView style={ styles.titleContainer }>
 				<ThemedText type="title">Messages</ThemedText>
 			</ThemedView>
 			<ThemedText>This app includes example code to help you get started.</ThemedText>

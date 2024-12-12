d1ec0fa951117cc396d37a5fa53d4158b99d0e27
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-12-12 12:04:39 +0100

    [FEATURE] Add dependancies NativeWind

--- app/(tabs)/messages.tsx
+++ app/(tabs)/messages.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
